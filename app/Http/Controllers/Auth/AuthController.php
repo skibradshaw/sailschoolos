ab2a9fc0a073c82b2cd8287693fa46bb01d0d2c3
Tim Bradshaw <tim@alltrips.com>
2016-01-26 12:41:03 -0700

    Fix for troubleshooting failed upgrade

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,6 +24,7 @@ class AuthController extends Controller
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
     
     protected $loginPath = '/login';
+    protected $redirectPath = '/';
 
     /**
      * Create a new authentication controller instance.
